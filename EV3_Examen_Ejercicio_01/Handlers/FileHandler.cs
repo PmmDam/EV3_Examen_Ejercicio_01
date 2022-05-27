b03c6e8788f29009c25fd4db1f0cce5136daf0a7
Pablo Muñoz Martínez <699328@alu.murciaeduca.es>
2022-05-27 20:46:50 +0200

    Add project files.

---
+++ EV3_Examen_Ejercicio_01/Handlers/FileHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV3_Examen_Ejercicio_01.Handlers
+{
+    public static class FileHandler
+    {
+
+        // 1. Crea un método asíncrono que escriba en un fichero, por líneas, los números del 1 al 1000.
+        // Haz que el método se ejecute en un camino de ejecución independiente paralelo(un thread del pool
+        // de threads) y finaliza el programa, sin esperar a que la ejecución del método termine.
+
+        public static async Task WriteTextAsync(string filePath,int limit)
+        {
+            int i = 0;
+            string[] contents = new string[limit+1];
+            
+            while (i <= limit)
+            {
+                contents[i] = i.ToString();   
+                i++;
+            }
+            await File.WriteAllLinesAsync(filePath,contents);
+        }
+
+
+
+    }
+}
---
+++ EV3_Examen_Ejercicio_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using EV3_Examen_Ejercicio_01.Handlers;
+
+namespace EV3_Examen_Ejercicio_01
+{
+    internal class Program
+    {
+
+        static string filePath = Path.Combine(Environment.CurrentDirectory, "Data", "Fichero.txt");
+        public static async Task Main(string[] args)
+        {
+            // 1. Crea un método asíncrono que escriba en un fichero, por líneas, los números del 1 al 1000.
+            // Haz que el método se ejecute en un camino de ejecución independiente paralelo(un thread del pool
+            // de threads) y finaliza el programa, sin esperar a que la ejecución del método termine.
+
+        
+         _=  Task.Run(async () => await FileHandler.WriteTextAsync(filePath, 1000)).ConfigureAwait(false);
+
+             
+        }
+    }
+}
